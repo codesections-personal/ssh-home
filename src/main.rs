a658bd45b44713497910b9dc687539c36b147cd0
Daniel Sockwell <dsockwell@gmail.com>
2019-12-15 14:19:15 -0500

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use boolinator::Boolinator;
 use clap::App;
 use run_script::{run_script, ScriptOptions};
 
@@ -7,30 +8,20 @@ fn main() {
         .arg("-p, --port=[PORT] 'Connect to PORT rather than the default")
         .get_matches();
 
-    let (_, mut pw, _) = run_script!(r##"echo "" | dmenu -p "Password: " -nf "#222222""##).unwrap();
-    pw.pop();
+    let dmenu_passwd_cmd = r##"echo -n $(echo "" | dmenu -p "Password: " -nf "#222222")"##;
+    let (_, password, _) = run_script!(dmenu_passwd_cmd).unwrap();
 
-    let (_, mut ip, _) = run_script!(format!(
-        r#"curl -u dsock:{} https://d5.codesections.com"#,
-        pw
-    ))
-    .unwrap();
-    ip.pop();
-
-    println!("Pass: {}\nIP: {}", pw, ip);
+    let d5_cmd = format!("curl -u dsock:{} https://d5.codesections.com", password);
+    let (_, ip, _) = run_script!(d5_cmd).unwrap();
 
     let mut options = ScriptOptions::new();
     options.capture_output = false; // False will print it to the parent process output.
-    run_script!(
-        format!(
-            r#"ssh -A -l dsock -R {}\
-               {}\
-                -p {}"#,
-            "/run/user/1000/gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra",
-            ip,
-            matches.value_of("port").unwrap_or("2222")
-        ),
-        &options
-    )
-    .unwrap();
+    let verbose = matches.is_present("verbose").as_some("-vvv").unwrap_or("");
+    let remote_socket = "/run/user/1000/gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra";
+    let port = matches.value_of("port").unwrap_or("2222");
+    let curl_cmd = format!(
+        "ssh -A -l dsock -R {} {} -p {} {}",
+        remote_socket, ip, port, verbose,
+    );
+    run_script!(curl_cmd, &options).unwrap();
 }
