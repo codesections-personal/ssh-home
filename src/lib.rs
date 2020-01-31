82e11d7ecf99d673b17d63feaf5e271d48efb739
Daniel Sockwell <daniel@codesections.com>
2020-01-31 17:29:06 -0500

    LTS

--- src/lib.rs
+++ src/lib.rs
@@ -1,114 +1,58 @@
-use derive_builder::Builder;
 use std::{error::Error, net::Ipv4Addr, os::unix::process::CommandExt, process::Command};
-use utils::{dependencies2, BoxedErr, CommandUtils};
+use utils::dependencies;
 
-#[derive(Builder, Debug)]
-#[builder(default)]
+#[derive(Debug)]
 pub struct SshHome<'a> {
-    ip: Option<Ipv4Addr>,
-    pub port: String,
-    username: String,
-    remote_socket: String,
-    verbose_flag: bool,
-    command: Option<&'a str>,
-}
-impl SshHomeBuilder<'_> {
-    pub fn new() -> Self {
-        Self::default()
-    }
+    ip: Ipv4Addr,
+    pub port: &'a str,
+    pub user: &'a str,
+    socket: &'a str,
+    pub verbose: bool,
+    pub command: Option<&'a str>,
 }
 
 impl<'a> SshHome<'a> {
-    fn build_cmd(self) -> Command {
-        let mut cmd = Command::new("ssh");
-        cmd.flag(Some("-A"))
-            .option(Some(&["-l", &self.username]))
-            .option(Some(&["-R", &self.remote_socket]))
-            .option(Some(&["-p", &self.port]))
-            .positional_arg(self.ip.map(|addr| addr.to_string()))
-            .flag(match self.verbose_flag {
-                true => Some("-vvv"),
-                false => None,
-            })
-            .positional_arg(self.command.map(String::from));
-        cmd
-    }
-    pub fn exec(self) -> Result<(), Box<dyn Error>> {
-        dependencies2(vec!["ssh"])?;
-        self.build_cmd().exec();
-        Ok(())
+    pub fn new(ip: Ipv4Addr) -> Self {
+        Self {
+            ip,
+            port: "2222",
+            user: "dsock",
+            socket: "/run/user/1000/gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra",
+            verbose: false,
+            command: None,
+        }
     }
 
+    pub fn exec(self) -> Result<(), Box<dyn Error>> {
+        self.build_cmd()?.exec();
+        Ok(())
+    }
     pub fn run(self) -> Result<(String, String), Box<dyn Error>> {
-        dependencies2(vec!["ssh"])?;
-        let output = self.build_cmd().output()?;
+        let output = self.build_cmd()?.output()?;
         if output.status.success() {
             Ok((
                 String::from_utf8(output.stdout)?,
                 String::from_utf8(output.stderr)?,
             ))
         } else {
-            Err(BoxedErr::new(&String::from_utf8(output.stderr)?))
+            Err(String::from_utf8(output.stderr)?.into())
         }
     }
-    // pub fn with_ip(self, ip: Ipv4Addr) -> Self {
-    //     Self {
-    //         ip: Some(ip),
-    //         ..self
-    //     }
-    // }
+    fn build_cmd(self) -> Result<Command, Box<dyn Error>> {
+        dependencies(vec!["ssh"])?;
 
-    // pub fn new() -> Self {
-    //     Self::default()
-    // }
-    // pub fn with_verbose_flag(self, verbose_flag: bool) -> Self {
-    //     Self {
-    //         verbose_flag,
-    //         ..self
-    //     }
-    // }
-    // pub fn with_port(self, port: Option<&str>) -> Self {
-    //     if let Some(port) = port {
-    //         Self {
-    //             port: port.to_string(),
-    //             ..self
-    //         }
-    //     } else {
-    //         self
-    //     }
-    // }
-    // pub fn with_username(self, username: Option<&str>) -> Self {
-    //     if let Some(username) = username {
-    //         Self {
-    //             username: username.to_string(),
-    //             ..self
-    //         }
-    //     } else {
-    //         self
-    //     }
-    // }
-    // pub fn with_command(self, command: Option<&'a str>) -> Self {
-    //     if let Some(command) = command {
-    //         Self {
-    //             command: Some(command),
-    //             ..self
-    //         }
-    //     } else {
-    //         self
-    //     }
-    // }
-}
-impl<'a> Default for SshHome<'a> {
-    fn default() -> Self {
-        Self {
-            ip: None,
-            port: "2222".to_string(),
-            username: "dsock".to_string(),
-            remote_socket:
-                "/run/user/1000/gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra"
-                    .to_string(),
-            verbose_flag: false,
-            command: None,
+        let mut cmd = Command::new("ssh");
+        cmd.arg("-A")
+            .args(&["-l", &self.user])
+            .args(&["-R", &self.socket])
+            .args(&["-p", &self.port])
+            .arg(self.ip.to_string());
+        if self.verbose {
+            cmd.arg("-vvv");
         }
+        if let Some(command) = self.command {
+            cmd.arg(command);
+        }
+        Ok(cmd)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::{crate_name, crate_version, App, ArgMatches};
 use d5_cli::D5;
-use ssh_home::SshHomeBuilder;
+use ssh_home::SshHome;
 use std::error::Error;
-use utils::{BoxedErr, Die};
+use utils::Die;
 
 fn main() {
     let cli = App::new(crate_name!())
@@ -13,27 +13,45 @@ fn main() {
         .arg("-P  --pass [PASSWORD] 'The password to use when getting the IP address from d5'")
         .arg("-c --command [COMMAND] 'Send command via ssh instead of connecting for interactive use'")
         .arg("--ip [IP_ADDRESS] 'The IP address to use (instead of getting it via d5)'")
+        .arg("--src 'Prints this program's source to stdout'")
         .get_matches();
 
     run(cli).unwrap_or_die();
 }
 
 fn run(cli: ArgMatches) -> Result<(), Box<dyn Error>> {
+    if cli.is_present("src") {
+        print!(
+            "/// main.rs\n{main}\n\n/// lib.rs\n{lib}",
+            main = include_str!("main.rs"),
+            lib = include_str!("lib.rs")
+        );
+        return Ok(());
+    }
     let ip: std::net::Ipv4Addr = match cli.value_of("ip") {
         Some(ip) => ip
             .parse()
-            .map_err(|_| BoxedErr::new(&format!("{} is not a valid IP address", ip)))?,
-        None => D5::new().with_password(cli.value_of("pass")).try_ip()?,
+            .map_err(|_| format!("{} is not a valid IP address", ip))?,
+        None => {
+            let mut d5 = D5::new();
+            d5.password = cli.value_of("pass");
+            d5.try_ip()?
+        }
     };
 
-    let mut ssh_home = SshHomeBuilder::new();
-    ssh_home
-        .ip(Some(ip))
-        .command(cli.value_of("command"))
-        .verbose_flag(cli.is_present("verbose"));
+    let mut ssh_home = SshHome::new(ip);
     if cli.is_present("verbose") {
-        ssh_home.verbose_flag(true);
+        ssh_home.verbose = true;
     }
-    ssh_home.build()?.exec()?;
+    if let Some(user) = cli.value_of("user") {
+        ssh_home.user = user;
+    }
+    if let Some(port) = cli.value_of("port") {
+        ssh_home.port = port;
+    }
+    if let Some(command) = cli.value_of("command") {
+        ssh_home.command = Some(command);
+    }
+    ssh_home.exec()?;
     Ok(())
 }
